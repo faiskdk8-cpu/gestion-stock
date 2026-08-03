741676d4407587b3fc8c9a6fd16e5a7e5e3c69e1
Yaouba <faiskdk8@gmail.com>
2026-08-03 13:01:14 +0200

    Fix CORS - add new Vercel URL

--- config/cors.php
+++ config/cors.php
@@ -6,12 +6,12 @@ return [
     'allowed_origins' => [
         'http://127.0.0.1:5173',
         'http://localhost:5173',
+        'https://gestion-stock-front-seven.vercel.app',
         'https://gestion-stock-front-q76jpmy7l-yaouba.vercel.app',
-        'https://gestion-stock-front.vercel.app',
     ],
     'allowed_origins_patterns' => ['https://.*\.vercel\.app'],
-    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
+    'allowed_headers' => ['*'],
 ];
