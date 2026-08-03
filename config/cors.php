385b44fda7194c14b00f35c7882888dfe729b69a
Yaouba <faiskdk8@gmail.com>
2026-08-03 13:45:07 +0200

    Open CORS for debugging

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,11 @@
 <?php
-
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://127.0.0.1:5173',
-        'http://localhost:5173',
-        'https://gestion-stock-front-seven.vercel.app',
-        'https://gestion-stock-front-q76jpmy7l-yaouba.vercel.app',
-    ],
-    'allowed_origins_patterns' => ['https://.*\.vercel\.app'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
-    'allowed_headers' => ['*'],
+    'supports_credentials' => false,
 ];
